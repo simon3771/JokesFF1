51271b5c76b3e4373ead1ff7510e52526d873b8e
Pio Molina <vpio@comcast.net>
2018-09-07 18:32:51 -0400

    should be mobile responsive

--- gotjokes/src/Credits.js
+++ gotjokes/src/Credits.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import PaperSheet2 from './PaperSheet2';
+import './css/paperSheetDiv.css';
 
 class Credits extends React.Component{
   render(){
 
     return(
-      <div>
+
+      <div id="something">
         <PaperSheet2 />
       </div>
+  
     )
   }
 }
