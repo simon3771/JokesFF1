bbec4ec70ee41e3f2896f278ac1ece163db1e912
Pio Molina <vpio@comcast.net>
2018-09-09 18:41:30 -0400

    stuck front page credit to footer

--- src/PaperSheet.js
+++ src/PaperSheet.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import './css/PaperSheet1.css';
 
 const styles = theme => ({
   root: {
@@ -15,7 +16,7 @@ function PaperSheet(props) {
   const { classes } = props;
 
   return (
-    <div>
+    <div className= "frontPageJokeCredit">
       <Paper className={classes.root} elevation={1}>
         <Typography component="p">
           All Jokes are courtesy of "The Official Joke API" made available by 15DKatz on GitHub
